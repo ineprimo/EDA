340282beb940970b1a4b745dda23f9498258b4c1
Cynthia Tristán <cyntrist@ucm.es>
2024-09-15 18:00:29 +0200

    tema 0

---
+++ 0-4/0-4.cpp
@@ -0,0 +1,47 @@
+// Nombre del alumno ..... Cynthia Tristán
+// Usuario del Juez ...... EDA-GDV73 
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+
+// función que resuelve el problema
+int resolver(int datos) {
+    return datos * 2;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    int datos = 0;
+    std::cin >> datos;
+    if (!std::cin) return false;
+    int sol = resolver(datos);
+    std::cout << sol << std::endl;
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    while (resuelveCaso())
+        ;
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
