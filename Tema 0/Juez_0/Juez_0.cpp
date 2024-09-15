fd7ce8dba0059ecc47137f7d7122c18fd5dfb64e
Paulalemany <palemany@ucm.es>
2024-09-15 15:15:46 +0200

    comienzo con el juez

---
+++ Tema 0/Juez_0/Juez_0.cpp
@@ -0,0 +1,9 @@
+// Juez_0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
