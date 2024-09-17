3d65747cfabfae185ae5f09a3a1a80cc94e53aa5
nievesag <nievalon@ucm.es>
2024-09-17 10:20:03 +0200

    .

--- 0/0-5/0-5.cpp
+++ 0/0-5/0-5.cpp
@@ -4,24 +4,57 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <vector>
 
-// función que resuelve el problema
-int resolver(int datos) 
+// funcioon que resuelve el problema
+std::vector<int> resolver(std::vector<int> datos)
 {
+    // recorre el vector de datos 
+    for(int i = 0; i < datos.size(); i++)
+    {
+        // reescribe cada elemento multiplicado x 2
+        datos[i] = datos[i] * 2;
+    }
 
+    // devuelve el vector solucion
+    return datos;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
+// -> esto se va a ejecutar una vez por caso
 void resuelveCaso() 
 {
     // leer los datos de la entrada
-    std::vector<int> v;
+    std::vector<int> datos;
+    int elementos;
+    int elem;
 
-    TipoSolucion sol = resolver(datos);
+    // Cada caso tiene dos lineas:
+    // La primera tiene un unico entero con el numero elementos de la secuencia de entrada
+    std::cin >> elementos;
+
+    // recorres esos elementos
+    for(int i = 0; i < elementos; i++)
+    {
+        // guardas el siguiente elemento del caso en elem
+        std::cin >> elem;
+
+        // lo metes en el vector de datos a multiplicar
+        datos.push_back(elem);
+    }
+
+    // guardas en el sol el vector ya modificado
+    std::vector<int> sol = resolver(datos);
+
     // escribir sol
 
+    for(int i = 0; i < elementos; i++)
+    {
+        std::cout << sol[i] << " ";
+    }
 
+    std::cout << std::endl;
 }
 
 int main() {
