f75ef1e4792c29a38f20dacc4ed455375d85cf7f
Andrga <andrga37@ucm.es>
2024-09-27 11:54:46 +0200

    2-7 casi

--- EDA2/EDA2/Tema2-07.cpp
+++ EDA2/EDA2/Tema2-07.cpp
@@ -1,40 +1,45 @@
 ﻿// Andres Garcia Navarro
 // EDA-GDV27
 
-/*
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <vector>
-
 using namespace std;
 
 // función que resuelve el problema
-bool resolver(const vector<int>& v, int ini, int fin) {
+int minimo(const vector<int>& sec, int ini, int fin) {
+	// Casos base
 	int dif = fin - ini;
-	// casos base
-	if (dif == 0)
-		return false;
-	if (dif == 1)
-		return v[ini] == ini;
+	if (dif == 0) {
+		return 0;
+	}
+	else if (dif == 1) {
+		return sec[ini];
+	}
+	else if (dif == 2) {
+		return (sec[ini] > sec[fin]) ? sec[ini] : sec[fin];
+	}
 
+	int mitad = (ini + fin) / 2,
+		menIzq = minimo(sec, ini, mitad),
+		menDer = minimo(sec, mitad, fin);
 
-	int mitad = (fin + ini) / 2;
-	if (resolver(v, ini, mitad))
-		return true;
-	else
-		return resolver(v, mitad, fin);
+	return menIzq > menDer ? menDer : menIzq;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
-void resuelveCaso() {
+bool resuelveCaso() {
 	// leer los datos de la entrada
 	int n;
 	cin >> n;
+	if (!cin) return false;
 	vector<int> sec(n);
 	for (int& e : sec) cin >> e;
-	cout << (resolver(sec, 0, n) ? "SI" : "NO") << endl;
+	cout << minimo(sec, 0, n) << endl;
+	return true;
 }
 
 int main() {
@@ -43,20 +48,17 @@ int main() {
 #ifndef DOMJUDGE
 	std::ifstream in("datos.txt");
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif 
+#endif
 
-
-	int numCasos;
-	std::cin >> numCasos;
-	for (int i = 0; i < numCasos; ++i)
-		resuelveCaso();
+	while (resuelveCaso())
+		;
 
 
 	// Para restablecer entrada. Comentar para acepta el reto
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
 	std::cin.rdbuf(cinbuf);
-	system("PAUSE");
+	//system("PAUSE");
 #endif
 
 	return 0;
-}*/
+}
