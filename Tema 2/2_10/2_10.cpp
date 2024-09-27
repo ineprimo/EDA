d7609c2eadfe3fa37bd470d33a70d757c9bbbff0
Paulalemany <palemany@ucm.es>
2024-09-27 15:48:33 +0200

    El 10 hecho
    No es especialmente eficiente creo pero no hay manera de descartar uno de los lados

--- Tema 2/2_10/2_10.cpp
+++ Tema 2/2_10/2_10.cpp
@@ -1,19 +1,77 @@
-// 2_10.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// función que resuelve el problema
+int resolver(const vector<int>& sec, int ini, int fin) {
+
+    //Comprobamos el número de elementos del vector
+
+    int resta = fin - ini;
+
+    if (resta == 0) {   //Solo hay un elemento en el trozo
+
+        //El elemento impar es ese elemento
+        if (sec[ini] % 2 != 0) return sec[ini];
+        else return 0;
+    }
+
+    if (resta == 1) {
+
+        //Comprobamos si uno de los dos es el elemento que buscamos
+        if (sec[ini] % 2 != 0) return sec[ini];
+        else if (sec[fin] % 2 != 0) return sec[fin];
+        else return 0;
+    }
+
+    //Si no es ninguno de los dos casos dividimos el vector
+
+    int mid = (ini + fin) / 2;
+    int izq = resolver(sec, ini, mid);
+
+    if (izq == 0) { //Si no está en la izquierda, miramos en la derecha
+        int der = resolver(sec, mid + 1, fin);
+        return der;
+    }
+    else return izq;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    // leer los datos de la entrada
+    int n;
+    cin >> n;
+
+    if (n == 0) return false;
+
+    vector<int> sec(n);
+    for (int& e : sec) cin >> e;
+
+    cout << resolver(sec, 0, n - 1) << endl;
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
