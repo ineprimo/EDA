a98cc64f43703858d7ab029f555c6c01d7f9c2d2
Paulalemany <palemany@ucm.es>
2024-10-01 20:32:56 +0200

    ESTOY AL DÍA
    Aunque he llegado a la conclusion de que los que tengo mal los voy a hacer cuando me aburra mucho

--- Tema 2/2_13/2_13.cpp
+++ Tema 2/2_13/2_13.cpp
@@ -1,19 +1,95 @@
-// 2_13.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// función que resuelve el problema
+int resolver(vector <int>& v, int ini, int fin) {
+    
+    //Casos base
+    if (ini == fin) return ini;
+
+    if (fin - ini == 2) {
+
+        if (v[ini + 1] == v[ini]) return fin;
+        return ini;
+    }
+
+    if (fin - ini == 1) {
+
+        //Por si está al final del vector
+        if (fin == v.size() - 1) {
+            if (v[ini - 1] == v[ini]) return fin;
+            return ini;
+        }
+
+        if (v[fin] == v[fin + 1]) return ini;
+        return fin;
+    }
+
+    int mid = 0;
+    mid = (ini + fin) / 2;
+
+    //La pareja está a la izquierda
+    if (v[mid - 1] == v[mid]) {
+
+        //Vamos hacia la izquierda
+        if (mid % 2 == 0) return resolver(v, ini, mid);
+        //Vamos a la derecha
+        return resolver(v, mid + 1, fin);
+    }
+    //La pareja está a la derecha
+    else if (v[mid] == v[mid + 1]) {
+
+        //Vamos a la derecha
+        if (mid % 2 == 0) return resolver(v, mid, fin);
+
+        return resolver(v, ini, mid);
+    }
+
+    else return mid;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int& e : v) cin >> e;
+
+    // Llamada a la función resolver
+    int sol = resolver(v, 0, n - 1);
+
+    // Mostrar el resultado
+    cout << sol << endl;
+}
+
+
+//#define DOMJUDGE
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
