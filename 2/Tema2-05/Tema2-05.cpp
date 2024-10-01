38fc8ebb7ecef73832d2c0461377e1b6b0b85846
ineprimo <ineprimo@ucm.es>
2024-10-01 15:24:59 +0200

    omfg era una bobadilla

--- 2/Tema2-05/Tema2-05.cpp
+++ 2/Tema2-05/Tema2-05.cpp
@@ -5,38 +5,68 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+//
+//pair<int, bool> caucasico(const vector<int>& sec, int ini, int fini) {
+// 
+//    // caso base
+//    if (fini - ini == 1) {
+//        if(sec[ini]%2 == 0)
+//            return { 1, true};
+//        return { 0, true};
+//    }
+//
+//
+//    // ----------------- recursion --------------
+//
+//    // der
+//    pair<int, bool> cauizq = caucasico(sec, ini, (ini+fini)/2);
+//    // izq
+//    pair<int, bool> cauder = caucasico(sec, ((ini+fini)/2), fini);
+//
+//
+//    // -------------- prepara la solucion -----------
+//
+//
+//    // caucasicoIzq.first && caucasicoDer.first && (abs(caucasicoIzq.second - caucasicoDer.second) <= 2)
+//    pair<int, bool> sol;
+//    sol.first = cauizq.first + cauder.first;
+//    if (cauizq.second && cauder.second && abs(cauizq.first - cauder.first) <= 2)
+//        sol.second = true;
+//    else
+//        sol.second = false;
+//
+//    //return sol;
+//
+//    return { cauder.first + cauizq.first,
+//             cauizq.second && cauder.second && (abs(cauizq.first - cauder.first) <= 2) };
+//
+//}
 
-pair<int, bool> caucasico(const vector<int>& sec, int ini, int fini) {
- 
-    // caso base
+
+// version guardando la cantidad de pares en n
+// devuelve si son o no son caucasicos
+bool caucasico(const vector<int>& sec, int ini, int fini, int& n ) {
+
+    // casos basicos, si la secuencia es 1 elem
     if (fini - ini == 1) {
+        if (sec[ini] % 2 == 0)
+            n = 1;
+        else
+            n = 0;
 
-        if(sec[ini]%2 == 0)
-            return { 1, true};
-        return { 0, true};
+        return true;    // si es de un elem siempre es caucasico
     }
 
+    int ni = 0, nd = 0;
 
-    // ----------------- recursion --------------
+    // recursion
+    int mid = (ini+fini) / 2;
+    bool ci = caucasico(sec, ini, mid, ni);     // llama a la parte izquierda
+    bool cd = caucasico(sec, mid, fini, nd);    // llama a la parte derecha
 
-    // der
-    pair<int, bool> cauizq = caucasico(sec, ini, (ini+fini)/2);
-    // izq
-    pair<int, bool> cauder = caucasico(sec, ((ini+fini)/2), fini);
+    n = ni + nd;
 
-
-    // -------------- prepara la solucion -----------
-
-
-    // caucasicoIzq.first && caucasicoDer.first && (abs(caucasicoIzq.second - caucasicoDer.second) <= 2)
-    pair<int, bool> sol;
-    sol.first = cauizq.first + cauder.first;
-    if (cauizq.second && cauder.second && abs(cauizq.first - cauder.first) <= 2)
-        sol.second = true;
-    else
-        sol.second = false;
-
-    return sol;
+    return ci && cd && abs(ni - nd) <= 2;
 
 }
 
@@ -50,7 +80,9 @@ bool resuelveCaso() {
     if (n == 0) return false;
     vector<int> sec(n);
     for (int& e : sec) cin >> e;
-    cout << (caucasico(sec, 0, sec.size()-1).second ? "SI" : "NO") << endl;
+
+    int a = 0;
+    cout << (caucasico(sec, 0, sec.size(), a) ? "SI" : "NO") << endl;
     return true;
 }
 
