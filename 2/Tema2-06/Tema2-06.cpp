03b68a8033b147d45c0076606b3e44d53a0128b7
ineprimo <ineprimo@ucm.es>
2024-10-01 16:21:04 +0200

    hola chaaaaaaaaaaaaaaaaaaaat

--- 2/Tema2-06/Tema2-06.cpp
+++ 2/Tema2-06/Tema2-06.cpp
@@ -7,8 +7,33 @@ using namespace std;
 
 // función que resuelve el problema
 bool resolver(const vector<int>& v, int ini, int fin) {
-    ...
+  
+    // caso base
+    if (fin - ini == 1) {
+        return v[ini] == ini;
+    }
+    if (fin - ini == 0)
+        return false;
+    
+    // recursion
+    int mid = (ini + fin) / 2;
+
+
+    // si el elemento en mid es mayor que mid todos los elementos de la derecha estan mal
+    if(v[mid] > mid)        
+        return resolver(v, ini, mid);
+    // si el elemento en mid es menor que mid todos los elementos de la izquierda estan mal
+    if(v[mid] < mid)       
+        return resolver(v, mid, fin);
+
+    // si mid == al elemento entonces devuelve true
+    return true;
+
+    // return
+    //return izq || der;
 }
+
+
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
---
+++ 2/Tema2-07/Tema2-07.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+// función que resuelve el problema
+int minimo(const vector<int>& sec, int ini, int fin) {
+    
+    // caso base
+    //if (fin - ini == 0) return 0;
+    if (fin - ini == 1) 
+        return sec[ini];
+
+    int mid = (ini+fin) / 2;
+
+    // recursion
+    int a = minimo(sec, ini, mid);
+    int b = minimo(sec, mid, fin);
+
+    //
+    if (a < b) return a;
+    return b;
+
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    cin >> n;
+    if (!cin) return false;
+    vector<int> sec(n);
+    for (int& e : sec) cin >> e;
+    cout << minimo(sec, 0, n) << endl;
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
