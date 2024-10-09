92327daa02287f24db4f7fade215971fc563047c
Paulalemany <palemany@ucm.es>
2024-10-09 20:03:57 +0200

    Clase Horas
    ¿terminada? falta probarla a ver que tal

--- Tema 4/4_2/Horas.h
+++ Tema 4/4_2/Horas.h
@@ -1,14 +1,53 @@
 #pragma once
 
 class Horas {
+public:
+	//operator <
+
+	Horas() {
+
+		horas = 0;
+		min = 0;
+		seg = 0;
+	}
+
+	Horas(int h, int m, int s) {
+
+		if (0 <= h <= 23 && 0 <= m <= 59 && 0 <= s <= 59)
+		{
+			horas = h;
+			min = m;
+			seg = s;
+		}
+		else throw "La hora no es adecuada";
+	}
+
+	const bool operator< (const Horas otro) {
+
+		//Si tiene menos horas directamente es menor
+		if (horas <= otro.horas) {
+
+			//Si tienen las mismas horas miramos los minutos
+			if (horas == otro.horas && min <= otro.min) {
+
+				if (min == otro.min && seg <= otro.seg) {
+
+					return seg == otro.seg;
+				}
+				else return true;
+			}
+			else return true;
+
+		}
+		else return false;
+		
+	}
+
+
 protected:
 
 	int horas;
 	int min;
 	int seg;
 
-public:
-
-	//operator <
-	//Constructora
 };
