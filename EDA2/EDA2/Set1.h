6126bdcbd6f6944ce4d428757a3e5451537d7b1e
anonymous <garcia.navarro.04.01@gmail.com>
2024-10-11 14:05:30 +0200

    y lo subo y me la pela y soy pro y lo termino en 1 h y punch

--- EDA2/EDA2/Set1.h
+++ EDA2/EDA2/Set1.h
@@ -8,10 +8,9 @@
 using namespace std;
 
 template <class T>
-class Set
-{
+class Set {
 protected:
-	static constexpr int TAM_INICIAL = 5; // tamaño inicial del array dinámico
+	static const int TAM_INICIAL = 5; // tamaño inicial del array dinámico
 
 	// número de elementos del array
 	int nelems;
@@ -23,33 +22,26 @@ protected:
 	T* array;
 
 public:
+
 	// constructor: conjunto vacío
-	Set() : nelems(0), capacidad(TAM_INICIAL), array(new T[capacidad])
-	{
-	}
+	Set() : nelems(0), capacidad(TAM_INICIAL), array(new T[capacidad]) {}
 
 	// constructor: conjunto vacío reservando espacio para initCap elementos
-	Set(int initCap) : nelems(0), capacidad(initCap), array(new T[capacidad])
-	{
-	}
+	Set(int initCap) : nelems(0), capacidad(initCap), array(new T[capacidad]) {}
 
 	// destructor
-	~Set()
-	{
+	~Set() {
 		libera();
 	}
 
 	// constructor por copia
-	Set(const Set& other)
-	{
+	Set(Set<T> const& other) {
 		copia(other);
 	}
 
 	// operador de asignación
-	Set& operator=(const Set& other)
-	{
-		if (this != &other)
-		{
+	Set<T>& operator=(Set<T> const& other) {
+		if (this != &other) {
 			libera();
 			copia(other);
 		}
@@ -57,8 +49,7 @@ public:
 	}
 
 	// Añadir un elemento. O(n), n=nelems
-	void add(const T& elem)
-	{
+	void add(T const& elem) {
 		bool found;
 		int pos;
 
@@ -73,8 +64,7 @@ public:
 
 		// Usando busq. binaria de librería STL
 		T* it = lower_bound(array, array + nelems, elem);
-		if (it == array + nelems || *it != elem)
-		{
+		if (it == array + nelems || *it != elem) {
 			pos = it - array; // Aritmética de punteros
 			shiftRightFrom(pos);
 			array[pos] = elem;
@@ -85,8 +75,7 @@ public:
 	}
 
 	// Borrar elemento elem. O(n), n=nelems
-	void remove(const T& elem)
-	{
+	void remove(T const& elem) {
 		bool found;
 		int pos;
 
@@ -98,8 +87,7 @@ public:
 
 		// Usando busq. binaria de librería STL
 		T* it = lower_bound(array, array + nelems, elem);
-		if (it != array + nelems && *it == elem)
-		{
+		if (it != array + nelems && *it == elem) {
 			pos = it - array; // Aritmética de punteros
 			shiftLeftFrom(pos);
 			--nelems;
@@ -107,86 +95,50 @@ public:
 	}
 
 	// Chequear pertenencia de e. O(log(n)), n=nelems
-	bool contains(const T& elem) const
-	{
+	bool contains(T const& elem) const {
 		bool found;
 		int pos;
 
-		/*binSearch(elem,found,pos);
-		return found;*/
+		binSearch(elem, found, pos);
+		return found;
 
 		// Usando busq. binaria de librería STL
-		return binary_search(array, array + nelems, elem);
+		// return binary_search(array, array+nelems, elem);
 	}
 
 	// Consultar si el conjunto está vacío
-	bool empty() const
-	{
+	bool empty() const {
 		return nelems == 0;
 	}
 
 	// Consultar tamaño. O(1)
-	int size() const
-	{
+	int size() const {
 		return nelems;
 	}
 
 	// Relación de equivalencia. O(n), n = nelems
-	bool operator==(Set& other) const
-	{
+	bool operator==(Set<T>& other) const {
 		if (nelems == other.nelems)
 			return std::equal(array, array + nelems, other->array);
-		return false;
+		else
+			return false;
 	}
-	// Complejidad lineal O(n), se recorre el array una vez + m veces el segundo
-	Set operator-(const Set& other) const
-	{
-		Set<T> aux(nelems);
 
-		int i = 0, j = 0, k = 0;
-
-		// bucle que añade los elementos pertenecientes al primero que no al segundo
-		while (i < nelems && j < other.nelems)
-		{
-			if (array[i] < other.array[j]) {
-				aux.array[k] = array[i];
-				i++;
-				k++;
-			}
-			else if (array[i] == other.array[j]) {
-				i++;
-				j++;
-			}
-			else {
-				j++;
-			}
-		}
-
-
-		// Buecle que añade los elementos restantes de cada array
-		while (i < nelems) {
-			aux.array[k] = array[i];
-			i++;
-			k++;
-		}
-
-		aux.nelems = k;
-
-		return aux;
-	}
+	// ------ Declaracion de la sobrecarga de operadores ------
+	Set<T> operator||(const Set<T>& other) const;
+	Set<T> operator&&(const Set<T>& other) const;
 
 	template <class E>
 	friend ostream& operator<<(ostream& out, const Set<E>& s);
 
 protected:
-	void libera()
-	{
+
+	void libera() {
 		delete[] array;
 	}
 
 	// this está sin inicializar
-	void copia(const Set& other)
-	{
+	void copia(Set const& other) {
 		capacidad = other.nelems + TAM_INICIAL;
 		nelems = other.nelems;
 		array = new T[capacidad];
@@ -194,8 +146,7 @@ protected:
 			array[i] = other.array[i];
 	}
 
-	void amplia(int nuevaCap)
-	{
+	void amplia(int nuevaCap) {
 		T* viejo = array;
 		capacidad = nuevaCap;
 		array = new T[capacidad];
@@ -204,8 +155,7 @@ protected:
 		delete[] viejo;
 	}
 
-	void binSearch(const T& x, bool& found, int& pos) const
-	{
+	void binSearch(const T& x, bool& found, int& pos) const {
 		// Pre: los size primeros elementos de array están ordenados
 		//      size >= 0
 
@@ -218,8 +168,7 @@ protected:
 		//        found es true si x esta en array[0..nelems-1]
 	}
 
-	int binSearchAux(const T& x, int a, int b) const
-	{
+	int binSearchAux(const T& x, int a, int b) const {
 		// Pre: array está ordenado entre 0 .. nelems-1
 		//      ( 0 <= a <= nelems ) && ( -1 <= b <= nelems ) && ( a <= b+1 )
 		//      todos los elementos a la izquierda de 'a' son <= x
@@ -229,8 +178,7 @@ protected:
 
 		if (a == b + 1)
 			p = a - 1;
-		else if (a <= b)
-		{
+		else if (a <= b) {
 			m = (a + b) / 2;
 			if (array[m] <= x)
 				p = binSearchAux(x, m + 1, b);
@@ -244,29 +192,107 @@ protected:
 		//       si x es menor que todos los elementos de array, devuelve -1
 	}
 
-	void shiftRightFrom(int i)
-	{
+	void shiftRightFrom(int i) {
 		for (int j = nelems; j > i; j--)
 			array[j] = array[j - 1];
 	}
 
-	void shiftLeftFrom(int i)
-	{
+	void shiftLeftFrom(int i) {
 		for (; i < nelems - 1; i++)
 			array[i] = array[i + 1];
 	}
+
+
 };
 
 template <class T>
-ostream& operator<<(ostream& out, const Set<T>& set)
-{
-	out << "";
+ostream& operator<<(ostream& out, Set<T> const& set) {
+	out << "{";
 	for (int i = 0; i < set.nelems - 1; i++)
-		out << set.array[i] << " ";
+		out << set.array[i] << ",";
 	if (set.nelems > 0) out << set.array[set.nelems - 1];
-	out << "";
+	out << "}";
 	return out;
 }
 
+
+// ----- Definicion de sobrecarga de operadores -----
+
+// Complejidad lineal O(n) ya que se ha dimensionado aux con el tamaño máximo al que puede llegar en este metodo.
+// Si hiciera falta redimensionar, el algoritmo se volveria cuadratico O(n^2).
+// Operador OR:
+template<class T>
+inline Set<T> Set<T>::operator||(const Set<T>& other) const {
+
+	Set<T> aux(nelems + other.nelems);
+
+	int i = 0, j = 0;
+
+	// bucle que añade los elementos comunes y no comunes
+	while (i < nelems && j < other.nelems)
+	{
+		if (array[i] == other.array[j])
+		{
+			aux.add(array[i]);
+			i++;
+			j++;
+		}
+		else if (array[i] > other.array[j]) {
+			aux.add(other.array[i]);
+			j++;
+		}
+		else
+		{
+			aux.add(array[i]);
+			i++;
+		}
+	}
+
+	// Buecle que añade los elementos restantes de cada array
+	while (i < nelems) {
+		aux.add(array[i]);
+		i++;
+	}
+	while (j < other.nelems) {
+		aux.add(other.array[i]);
+		i++;
+	}
+
+
+	return aux;
+}
+
+// Complejidad lineal O(n) ya que se ha dimensionado aux con el tamaño máximo al que puede llegar en este metodo.
+// Si hiciera falta redimensionar, el algoritmo se volveria cuadratico O(n^2).
+// Operador AND:
+template<class T>
+inline Set<T> Set<T>::operator&&(const Set<T>& other) const {
+	Set<T>* mayor, menor;
+
+	// Asignamos el tamaño del set menor
+	Set<T> aux(nelems >= other.nelems ? other.nelems: nelems);
+
+
+	// bucle que añade los elementos comunes
+	int i = 0, j =0;
+	while (i < nelems && j < other.nelems)
+	{
+		if (array[i] == other.array[j])
+		{
+			aux.add(array[i]);
+			i++;
+			j++;
+		}
+		else if (array[i] > other.array[j]) {
+			j++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+		return aux;
+}
 
 #endif //SET_H
--- EDA2/EDA2/Tema4-05.cpp
+++ EDA2/EDA2/Tema4-05.cpp
@@ -1,60 +1,60 @@
-﻿// Andres Garcia Navarro
-// EDA-GDV27
-
-
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <vector>
-
-#include "Set.h"
-using namespace std;
-
-
-// Resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
-bool resuelveCaso() {
-	// leer los datos de la entrada
-	int d;
-	std::cin >> d;
-	if (!std::cin)
-		return false;
-	Set<int> set1(d);
-	for (int i = 0; i < d; i++) {
-		int n = 0;
-		cin >> n;
-		set1.add(n);
-	}
-
-	std::cin >> d;
-	Set<int> set2(d);
-	for (int i = 0; i < d; i++) {
-		int n = 0;
-		cin >> n;
-		set2.add(n);
-	}
-	cout << (set1 || set2) << endl << (set1 && set2) << endl;
-	return true;
-}
-
-int main() {
-	// Para la entrada por fichero.
-	// Comentar para acepta el reto
-#ifndef DOMJUDGE
-	std::ifstream in("datos.txt");
-	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif
-
-
-	while (resuelveCaso())
-		;
-
-
-	// Para restablecer entrada. Comentar para acepta el reto
-#ifndef DOMJUDGE // para dejar todo como estaba al principio
-	std::cin.rdbuf(cinbuf);
-	system("PAUSE");
-#endif
-
-	return 0;
-}
+﻿//// Andres Garcia Navarro
+//// EDA-GDV27
+//
+//
+//#include <iostream>
+//#include <iomanip>
+//#include <fstream>
+//#include <vector>
+//
+//#include "Set.h"
+//using namespace std;
+//
+//
+//// Resuelve un caso de prueba, leyendo de la entrada la
+//// configuración, y escribiendo la respuesta
+//bool resuelveCaso() {
+//	// leer los datos de la entrada
+//	int d;
+//	std::cin >> d;
+//	if (!std::cin)
+//		return false;
+//	Set<int> set1(d);
+//	for (int i = 0; i < d; i++) {
+//		int n = 0;
+//		cin >> n;
+//		set1.add(n);
+//	}
+//
+//	std::cin >> d;
+//	Set<int> set2(d);
+//	for (int i = 0; i < d; i++) {
+//		int n = 0;
+//		cin >> n;
+//		set2.add(n);
+//	}
+//	cout << (set1 || set2) << endl << (set1 && set2) << endl;
+//	return true;
+//}
+//
+//int main() {
+//	// Para la entrada por fichero.
+//	// Comentar para acepta el reto
+//#ifndef DOMJUDGE
+//	std::ifstream in("datos.txt");
+//	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+//#endif
+//
+//
+//	while (resuelveCaso())
+//		;
+//
+//
+//	// Para restablecer entrada. Comentar para acepta el reto
+//#ifndef DOMJUDGE // para dejar todo como estaba al principio
+//	std::cin.rdbuf(cinbuf);
+//	system("PAUSE");
+//#endif
+//
+//	return 0;
+//}
