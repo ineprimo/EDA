ac80ed61c814f6d6fbe25ae75f839652c1831b56
Andrés García Navarro <andrga37@ucm.es>
2024-10-22 11:23:30 +0200

    hasta 5-01

--- EDA2/EDA2/Tema4-07.cpp
+++ EDA2/EDA2/Tema4-07.cpp
@@ -1,58 +1,58 @@
-﻿// Andres Garcia Navarro
-// EDA-GDV27
-
-
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include "Set.h"
-using namespace std;
-
-
-
-
-// Resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
-bool resuelveCaso() {
-    int n = 0;
-    std::cin >> n;
-    if (!std::cin)
-        return false;
-
-    Set<int> set1, set2;
-    int c = 0;
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> c;
-        set1.add(c);
-    }
-
-    std::cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> c;
-        set2.add(c);
-    }
-
-    cout << (set1 <= set2) << endl;
-    return true;
-}
-
-int main() {
-    // Para la entrada por fichero.
-    // Comentar para acepta el reto
-#ifndef DOMJUDGE
-    std::ifstream in("datos.txt");
-    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif
-
-    while (resuelveCaso())
-        ;
-
-    // Para restablecer entrada. Comentar para acepta el reto
-#ifndef DOMJUDGE // para dejar todo como estaba al principio
-    std::cin.rdbuf(cinbuf);
-#endif
-
-    return 0;
-}
+﻿//// Andres Garcia Navarro
+//// EDA-GDV27
+//
+//
+//#include <iostream>
+//#include <iomanip>
+//#include <fstream>
+//#include "Set.h"
+//using namespace std;
+//
+//
+//
+//
+//// Resuelve un caso de prueba, leyendo de la entrada la
+//// configuración, y escribiendo la respuesta
+//bool resuelveCaso() {
+//    int n = 0;
+//    std::cin >> n;
+//    if (!std::cin)
+//        return false;
+//
+//    Set<int> set1, set2;
+//    int c = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        std::cin >> c;
+//        set1.add(c);
+//    }
+//
+//    std::cin >> n;
+//    for (int i = 0; i < n; i++)
+//    {
+//        std::cin >> c;
+//        set2.add(c);
+//    }
+//
+//    cout << (set1 <= set2) << endl;
+//    return true;
+//}
+//
+//int main() {
+//    // Para la entrada por fichero.
+//    // Comentar para acepta el reto
+//#ifndef DOMJUDGE
+//    std::ifstream in("datos.txt");
+//    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+//#endif
+//
+//    while (resuelveCaso())
+//        ;
+//
+//    // Para restablecer entrada. Comentar para acepta el reto
+//#ifndef DOMJUDGE // para dejar todo como estaba al principio
+//    std::cin.rdbuf(cinbuf);
+//#endif
+//
+//    return 0;
+//}
