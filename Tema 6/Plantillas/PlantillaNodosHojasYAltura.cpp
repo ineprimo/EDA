9f446afeb4cb5490ed44d0d1f2a9fc9045a909f7
Paigro <paigro9@gmail.com>
2024-11-05 11:34:20 +0100

    Tema 6 plantillas.

---
+++ Tema 6/Plantillas/PlantillaNodosHojasYAltura.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "bintree_eda.h"
+using namespace std;
+
+template <class T>
+int nodos(bintree<T> const& tree){
+    ...
+}
+
+...
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    bintree<char> tree;
+    tree = leerArbol('.');
+    cout << nodos(tree) << " " << hojas(tree) << " " << altura(tree) << endl;
+}
+
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
