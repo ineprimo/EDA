68a063e06d0a272a824acf4b301d8157b06a9be7
Paulalemany <palemany@ucm.es>
2024-11-07 11:09:46 +0100

    Listo todo para el tema 6

---
+++ Plantillas/tema_6/PlantillaTramosNavegables.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include "bintree_eda.h"
+using namespace std;
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso(){
+	bintree<int> arb;
+	arb = leerArbol(-1); // -1 es la repr. de arbol vacio
+	...
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
+
+    return 0;
+}
+
