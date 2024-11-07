675709a9e361b6fb0c24a904f2328ccbac5dd00d
Paigro <paigro9@gmail.com>
2024-11-07 19:41:05 +0100

    6-5 mal. Voy a intentarlo de otra forma.

--- Tema 6/6-5/6-5/6-5.cpp
+++ Tema 6/6-5/6-5/6-5.cpp
@@ -8,11 +8,50 @@
 #include "bintree_eda.h"
 
 
-// COMPLEJIDAD: .
+// COMPLEJIDAD: O(n) siendo n el numero de elementos del arbol.
 template <class T>
-void busqueda(const bintree<T>& tree)
+bool busqueda(const bintree<T>& tree)
 {
+	// Caso base esta vacio.
+	if (tree.empty())
+	{
+		return true;
+	}
 
+	// Auxiliares. Suponemos que lo son de busqueda.
+	bool esBusquedaIzq = true; // Auxiliar para guardar si el brazo izquierdo es de busqueda.
+	bool esBusquedaDer = true; // Auxiliar para guardar si el brazo derecho es de busqueda.
+
+	// Rama izquierda. Solo si no esta vacia.
+	if (!tree.left().empty())
+	{
+		// Miramos si su hijo es de busqueda si cumple que el elemento de su hijo izquierdo es menor que el propio.
+		if (tree.root() > tree.left().root())
+		{
+			esBusquedaIzq = busqueda(tree.left());
+		}
+		else // Sino no lo puede ser.
+		{
+			esBusquedaIzq = false;
+		}
+	}
+
+	// Rama derecha. Solo si no esta vacia.
+	if (!tree.right().empty())
+	{
+		// Miramos si su hijo es de busqueda si cumple que el elemento de su hijo derecho es mayor que el propio.
+		if (tree.root() < tree.right().root())
+		{
+			esBusquedaDer = busqueda(tree.right());
+		}
+		else // Sino no lo puede ser.
+		{
+			esBusquedaDer = false;
+		}
+	}
+
+	// Es de busqueda si ambos hijos son de busqueda.
+	return esBusquedaIzq && esBusquedaDer;
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
@@ -23,10 +62,14 @@ void resuelveCaso()
 
 	tree = leerArbol(-1);
 
-	busqueda(tree);
-
-	std::cout << "TO DO" << std::endl;
-
+	if (busqueda(tree))
+	{
+		std::cout << "SI" << std::endl;
+	}
+	else
+	{
+		std::cout << "NO" << std::endl;
+	}
 }
 
 int main()
