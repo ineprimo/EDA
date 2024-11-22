cf0d021546dc84b26abef9105b62ed90fdafa0d9
ineprimo <ineprimo@ucm.es>
2024-11-22 19:52:32 +0100

    MIKIIIIIIIIIIIIIIII AGUARDAME

--- 7/7/Tema7-6/Tema7-6.cpp
+++ 7/7/Tema7-6/Tema7-6.cpp
@@ -3,23 +3,27 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <unordered_map>
 using namespace std;
 
 
 using Actor = pair<string, int>;        // nombre mins
-using Pelicula = map<string, int>; // nombre emision
+using Pelicula = string; // nombre emision
 using Actores = map<string, int>; // nombre emision
 
 using RepartosPeliculas = map<string, vector<Actor>>; //
 
+using Solution = map<pair<Pelicula, int>, pair<int, map<string, int>>>;
+
 
 void leerRepartos(int numPeliculas, RepartosPeliculas& peliculas) {
 
-    int numActores;
-    Actor actor;
+
+    Pelicula peli; int numActores;
+    Actor actor; int minutos = 0;
+
     string nombreA, nombreP;
-    int min;
-    vector<Actor> aux;
+    vector<Actor> actores;
 
     for (int i = 0; i < numPeliculas; ++i) {
         // ---------------- ACTORES ---------------------
@@ -29,55 +33,98 @@ void leerRepartos(int numPeliculas, RepartosPeliculas& peliculas) {
         // peli.first = nombreP;
          // rellena el vector de actores
         for (int j = 0; j < numActores; ++j) {
-            cin >> nombreA >> min;
-            Actor a = Actor(nombreA, min);
+            cin >> nombreA >> minutos;
+            Actor a = Actor(nombreA, minutos);
 
-            aux.push_back(a);
+            actores.push_back(a);
         }
-        peliculas.insert({ nombreP, aux });
-        aux.clear();
-
+        peliculas.insert({ {nombreP, 0}, actores });
+        actores.clear();
     }
-
-    peliculas;
-
 }
 
+ Solution procesarEmisiones(RepartosPeliculas const& repartos, vector<string> const& secEmisiones) {
 
-void procesarEmisiones(RepartosPeliculas const& repartos, vector<string> const& secEmisiones) {
-
-    // ----- PELI -----
-    // pelicula mas emitida + veces emitida
-    Pelicula p;      // Pelicula mas emitida
-    for (int i = 0; i < secEmisiones.size(); i++) {
-        if (p.count(secEmisiones[i]))
-            p.insert({ secEmisiones[i], 1 });
-        else
-            p[secEmisiones[i]]++;
-    }
-
-    Actores a;
-    for (int i = 0; i < secEmisiones.size(); i++) {
-
-        vector<Actor> aux = (repartos.find(secEmisiones[i]))->second;
-        for (int j = 0; j > aux.size(); j++) {
-            if (!a.count(aux[j].first)){
-                a.insert(aux[j]); // {aux[j].first, aux[j].second}
-            }
-            else {
-                a[aux[j].first] += aux[j].second;
-            }
-        }
-    }
-
-   
-}
+     // sol_pelicula, pair<mins, sol_actores>
+     Solution sol;
 
 
-void procesarEmisiones2(RepartosPeliculas const& repartos, vector<string> const& secEmisiones) {
+     pair<Pelicula, int> sol_peliculas = { "", 0 };
+     unordered_map <Pelicula, int> pelis;
+
+     pair<string, int> sol_actores_aux = { "",0 };
+     map<string, int> actores;
+     map<string, int> sol_actores;
+
+     // contador de cuantas veces aparece
+     for (auto e : secEmisiones) {
+
+         // pelicula actual
+         auto it = pelis.find(e);
+
+         // reparto de la peli
+         auto i = repartos.find(e);
+
+         // si no esta en pelis
+         if (it == pelis.end()) {
+             pelis.insert({e, 1});
+
+             // recorre el reparto
+             for (auto a : i->second) {
+
+                 // coge el actor
+                 auto actor = actores.find(a.first);
+
+                 // si no esta en actores
+                 if (actor == actores.end()) {
+                     actores.insert(a);
+                 }
+                 else {
+                     actor->second += a.second;
+                 }
+             }
+         }
+         else {
+             // aumenta las emisiones
+             it->second++;
+
+             // recorre el reparto
+             for (auto a : i->second) {
+
+                 // coge el actor
+                 auto actor = actores.find(a.first);
+                 actor->second += a.second;
+             }
+         }
 
 
+         // contador de emisiones
+         it = pelis.find(e);
+         if (it->second >= sol_peliculas.second) {
+             sol_peliculas = *it;
+         }
+     }
 
+     // busca al actor con mas screentime
+     for (auto a : actores) {
+
+         if (sol_actores_aux.second < a.second) {
+             sol_actores_aux = a;
+
+             // corrige el mapa solucion
+             sol_actores.clear();
+             sol_actores.insert(sol_actores_aux);
+         }
+         else if (sol_actores_aux.second == a.second) {
+             sol_actores.insert(a);
+         }
+     }
+
+
+     pair<int, map<string, int>> aux = {sol_actores_aux.second, sol_actores};
+     sol.insert({sol_peliculas, aux});
+
+     return sol;
 }
 
 
@@ -103,7 +150,21 @@ bool resuelveCaso() {
 
     }
 
-    procesarEmisiones(repartos, secEmisiones);
+    Solution sol = procesarEmisiones(repartos, secEmisiones);
+
+
+    // using Solution = map<pair<Pelicula, int>, pair<int, map<string, int>>>;
+    
+    for (auto p : sol) {
+        cout << p.first.second << " " << p.first.first << endl;
+        cout << p.second.first;
+        for (auto a : p.second.second) {
+
+            cout << " " << a.first;
+        }
+        cout << endl;
+    }
+    
 
     return true;
 }
