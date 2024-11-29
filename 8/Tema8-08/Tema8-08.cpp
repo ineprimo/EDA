547f2835a8b219f2ee5c6f52667cb9b4388f5287
(i)nés <ineprimo@ucm.es>
2024-11-29 16:32:20 +0100

    unordered_set te odio

---
+++ 8/Tema8-08/Tema8-08.cpp
@@ -0,0 +1,237 @@
+/*
+Nombre completo:
+DNI:
+Usuario del juez:
+Puesto de laboratorio:
+Qué has conseguido hacer y qué no:
+*/
+
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+using Heroe = string;
+using Villano = string;
+
+struct Attack {
+    string name;
+    int dmg;
+};
+
+struct Character {
+    int hp;
+    string name;
+    bool dead;
+
+    // set para gestionar los ataques, no tienen prioridad pero no se pueden suplicar
+    set<Attack> attacks;
+};
+
+struct Villain : public Character {
+
+};
+
+struct Hero : public Character {
+    // ??
+
+};
+
+class SistemaBatallas {
+private:
+    // ...
+
+    // cola para la gestion de turnos
+    queue<Character> turns;
+
+    // set no ordenado para la gestion de personajes activos
+    unordered_set<string> characters;
+
+public:
+    // Coste:
+    void aparece_villano(Villano const& v, int puntos, int valor) {
+       
+        auto aux = characters.find(v);
+
+        // si esta
+        if (*aux == v) {
+            throw invalid_argument("Personaje ya existente");
+        }
+        Villain villain = Villain();
+        villain.name = v;
+        villain.hp = puntos;
+        villain.dead = false;
+
+       /* Attack a = Attack();
+        a.name = "default";
+        a.dmg = valor;*/
+
+        //villain.attacks.insert(a);
+
+        turns.push(villain);
+        characters.insert(v);
+    }
+
+    // Coste:
+    void aparece_heroe(Heroe const& h, int puntos) {
+
+        auto aux = characters.find(h);
+
+        // si esta
+        if (*aux == h) {
+            throw invalid_argument("Personaje ya existente");
+        }
+        Hero hero = Hero();
+        hero.name = h;
+        hero.hp = puntos;
+        hero.dead = false;
+
+        turns.push(hero);
+        characters.insert(h);
+
+    }
+
+    // Coste:
+    void aprende_ataque(Heroe const& h, string const& ataque, int valor) {
+        //...
+        //    throw invalid_argument("Heroe inexistente");
+        //...
+        //    throw invalid_argument("Ataque repetido");
+        //...
+    }
+
+    // Coste:
+    vector<pair<string, int>> mostrar_ataques(Heroe const& h) {
+        //...
+        //    throw invalid_argument("Heroe inexistente");
+        //...
+        vector<pair<string, int>> res;
+        //...
+        return res;
+    }
+
+    // Coste:
+    vector<pair<string, int>> mostrar_turnos() {
+        vector<pair<string, int>> res;
+        // ...
+            return res;
+    }
+
+    // Coste:
+    bool villano_ataca(Villano const& v, Heroe const& h) {
+        //...
+        //    throw invalid_argument("Villano inexistente");
+        //...
+        //    throw invalid_argument("Heroe inexistente");
+        //...
+        //   throw invalid_argument("No es su turno");
+        //...
+        return true;
+    }
+
+    // Coste:
+    bool heroe_ataca(Heroe const& h, string const& ataque, Villano const& v) {
+        //...
+        //    throw invalid_argument("Villano inexistente");
+        //...
+        //    throw invalid_argument("Heroe inexistente");
+        //...
+        //    throw invalid_argument("No es su turno");
+        //...
+        //    throw invalid_argument("Ataque no aprendido");
+        //...
+        return true;
+    }
+
+};
+
+
+bool resuelveCaso() { // No tacar nada de esta función!
+    string comando;
+    cin >> comando;
+    if (!cin) return false;
+
+    SistemaBatallas sistema;
+    Villano v; Heroe h;
+    string ataque; int ptos; int valor;
+    while (comando != "FIN") {
+        try {
+            if (comando == "aparece_villano") {
+                cin >> v >> ptos >> valor;
+                sistema.aparece_villano(v, ptos, valor);
+            }
+            else if (comando == "aparece_heroe") {
+                cin >> h >> ptos;
+                sistema.aparece_heroe(h, ptos);
+            }
+            else if (comando == "aprende_ataque") {
+                cin >> h >> ataque >> valor;
+                sistema.aprende_ataque(h, ataque, valor);
+            }
+            else if (comando == "mostrar_ataques") {
+                cin >> h;
+                auto ataques = sistema.mostrar_ataques(h);
+                cout << "Ataques de " << h << endl;
+                for (int i = 0; i < ataques.size(); i++) {
+                    ataque = ataques[i].first;
+                    valor = ataques[i].second;
+
+                    cout << ataque << " " << valor << "\n";
+                }
+            }
+            else if (comando == "mostrar_turnos") {
+                cout << "Turno: " << endl;
+
+                // vector<pair<string, int>>
+                string pers; int puntos;
+                auto turnos = sistema.mostrar_turnos();
+                for (int i = 0; i < turnos.size(); i++) {
+                    pers = turnos[i].first;
+                    puntos = turnos[i].second;
+
+                    cout << pers << " " << puntos << "\n";
+                }
+            }
+            else if (comando == "villano_ataca") {
+                cin >> v >> h;
+                bool derrotado = sistema.villano_ataca(v, h);
+                cout << v << " ataca a " << h << endl;
+                if (derrotado) cout << h << " derrotado" << endl;
+            }
+            else if (comando == "heroe_ataca") {
+                cin >> h >> ataque >> v;
+                bool derrotado = sistema.heroe_ataca(h, ataque, v);
+                cout << h << " ataca a " << v << endl;
+                if (derrotado) cout << v << " derrotado" << endl;
+            }
+        }
+        catch (std::exception& e) {
+            cout << "ERROR: " << e.what() << "\n";
+        }
+        cin >> comando;
+    }
+
+    cout << "---\n";
+    return true;
+}
+
+//#define DOMJUDGE
+int main() {
+#ifndef DOMJUDGE
+    ifstream in("prueba.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+    while (resuelveCaso()) {}
+
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+#endif
+    return 0;
+}
