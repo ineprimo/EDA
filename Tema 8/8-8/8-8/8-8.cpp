a1da8ba36c5a0247a94bbc0d71ba255fb6533c78
anonymous <paigro9@gmail.com>
2024-12-03 14:19:05 +0100

    8-8 acabado.
    Gracias Andrés te quiero. <3

--- Tema 8/8-8/8-8/8-8.cpp
+++ Tema 8/8-8/8-8/8-8.cpp
@@ -10,176 +10,302 @@ Usuario del juez: EDA-GDV35.
 #include <vector>
 #include <utility>
 #include <map>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+#include <algorithm>
 
 
-using namespace std;
+//using namespace std; // PAIGRO AQUI: quitar luego.
+using Respuesta = std::vector<std::pair<std::string, int>>;
+
+using Heroe = std::string; // Nombre del heroe.
+using Villano = std::string; // Nombre del villano.
 
 
-using Heroe = string;
-using Villano = string;
-
-
-struct Vill
+class SistemaBatallas
 {
-public:
-	Villano nombre;
-	int vida;
-	int danyo;
-};
-
-struct Her
-{
-public:
-	Heroe nombre;
-};
-
-
-using Villanos = std::map<Vill, int>;
-using ataques = std::pair<std::string, int>;
-
-
-
-
-
-
-class SistemaBatallas {
 private:
-	Villanos villanos;
-	Her heroe;
+
+	struct InfoVillano // Struct que contiene la informacion del villano (vida y danyo).
+	{
+	public:
+
+		int vida; // Vida del villano.
+		int danyo; // Danyo que hace el villano.
+	};
+
+	struct InfoHeroe // Struct que contiene la informacion del heroe (vida y diferentes ataques que puede hacer).
+	{
+	public:
+
+		int vida; // Vida del heroe.
+		std::unordered_map<std::string, int> ataques; // Mapa no ordenado que contiene los diferentes ataques del heroe.
+	};
+
+	std::unordered_map<Villano, InfoVillano> villanos; // Mapa no ordenado con los villanos con su informacion.
+	std::unordered_map<Heroe, InfoHeroe> heroes; // Mapa no ordenado que contiene los heroes con su informacion.
+
+	std::queue<std::string> turnos; // Cola para ver a quien le toca el turno.
 
 public:
-	// Coste:
+
+	// Coste: O(1) en lel caso mejor pero O(n) si hay muchos villanos (n).
 	void aparece_villano(Villano const& v, int puntos, int valor)
 	{
-		if () {
-
+		// Si no esta el v lo mete.
+		if (!villanos.count(v))
+		{
+			villanos.insert({ v,{puntos, valor } });
+			turnos.push(v);
+		}
+		else // Sino excepcion.
+		{
+			throw std::invalid_argument("Personaje ya existente");
 		}
 	}
 
-	// Coste:
-	void aparece_heroe(Heroe const& h, int puntos) {
-
-		throw invalid_argument("Personaje ya existente");
-
+	// Coste: O(1) en el caso mejor pero O(n) si hay muchos heroes (n).
+	void aparece_heroe(Heroe const& h, int puntos)
+	{
+		// Si no esta el h lo mete.
+		if (!heroes.count(h))
+		{
+			heroes.insert({ h,{puntos} });
+			turnos.push(h);
+		}
+		else // Sino excepcion.
+		{
+			throw std::invalid_argument("Personaje ya existente");
+		}
 	}
 
-	// Coste:
-	void aprende_ataque(Heroe const& h, string const& ataque, int valor) {
-
-		throw invalid_argument("Heroe inexistente");
-
-		throw invalid_argument("Ataque repetido");
-
+	// Coste: O(1) en el caso mejor pero O(n+m) si hay muchos heroes (n) y villanos (m).
+	void aprende_ataque(Heroe const& h, std::string const& ataque, int valor)
+	{
+		if (!heroes.count(h)) // Si no esta el heroe no le mete el ataque y excepcion.
+		{
+			throw std::invalid_argument("Heroe inexistente");
+		}
+		else // Sino miramos si no lo tiene
+		{
+			InfoHeroe& inHe = heroes[h]; // Cogemos el struct del heroe.
+			if (!inHe.ataques.count(ataque)) // Si no esta el ataque lo metemos.
+			{
+				inHe.ataques.insert({ ataque, valor });
+			}
+			else // Sino excepcion. 
+			{
+				throw std::invalid_argument("Ataque repetido");
+			}
+		}
 	}
 
-	// Coste:
-	vector<pair<string, int>> mostrar_ataques(Heroe const& h) {
+	// Coste: O(n) siendo n el numero de ataques de h.
+	Respuesta mostrar_ataques(Heroe const& h)
+	{
+		if (!heroes.count(h)) // Si no existe el heroe excepcion.
+		{
+			throw std::invalid_argument("Heroe inexistente");
+		}
 
-		throw invalid_argument("Heroe inexistente");
+		Respuesta res; // Vector de ataques.
 
-		vector<pair<string, int>> res;
+		for (auto a : heroes[h].ataques)
+		{
+			res.push_back(a);
+		}
+
+		// Ordenacion alfabeticamente.
+		std::sort(res.begin(), res.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {return a.second > b.second; });
 
 		return res;
 	}
 
-	// Coste:
-	vector<pair<string, int>> mostrar_turnos() {
-		vector<pair<string, int>> res;
+	// Coste: O(n) en el caso mejor siendo n el numero de turnos 
+	// pero en el caso peor O(n*(h+v)) siendo n el numero de turnos, h el de heroes y v el de villanos.
+	Respuesta mostrar_turnos()
+	{
+		Respuesta res;
+
+		for (int i = 0; i < turnos.size(); i++)
+		{
+			auto it = turnos.front(); // Iterador al principio para recorrer.
+			std::pair<std::string, int> personaje; // Nos guardamos el personaje al que le toca el turno.
+
+			if (villanos.count(it)) // Si el iterador es un villanos que esta lo guardamos.
+			{
+				personaje = { it, villanos[it].vida };
+			}
+			else if (heroes.count(it)) // Si es un heroe que esta lo guardamos.
+			{
+				personaje = { it, heroes[it].vida };
+			}
+
+			turnos.push(it); // Pone el principio en el final para que sea ciclico.
+			turnos.pop(); // Quita el primero para que no se repita.
+
+			res.push_back(personaje); // Lo mete en la respuesta.
+		}
 
 		return res;
 	}
 
-	// Coste:
-	bool villano_ataca(Villano const& v, Heroe const& h) {
+	// Coste: O(n) en el caso mejor siendo n el numero de turnos
+	// pero en el caso peor O(n+h+n) siendo n el numero de turnos, h el numero de heroes y v el de villanos.
+	bool villano_ataca(Villano const& v, Heroe const& h)
+	{
+		if (!villanos.count(v)) // Si no esta el villano excepcion.
+		{
+			throw std::invalid_argument("Villano inexistente");
+		}
+		if (!heroes.count(h)) // Si no esta el heroe excepcion.
+		{
+			throw std::invalid_argument("Heroe inexistente");
+		}
+		if (turnos.front() != v) // Si no es el turno del villano excepcion.
+		{
+			throw std::invalid_argument("No es su turno");
+		}
 
-		throw invalid_argument("Villano inexistente");
+		heroes[h].vida -= villanos[v].danyo; // Realizamos el ataque.
 
-		throw invalid_argument("Heroe inexistente");
 
-		throw invalid_argument("No es su turno");
+		// Siguiente turno.
+		turnos.pop();
+		turnos.push(v);
 
+
+		// Si el heroe se ha muerto.
+		if (heroes[h].vida <= 0)
+		{
+			heroes.erase(h); // Quitamos h de heroes.
+			death(h); // Reseteamos turnos.
+			return true;
+		}
+		return false;
 	}
 
-	// Coste:
-	bool heroe_ataca(Heroe const& h, string const& ataque, Villano const& v) {
+	// Coste: O(n) en el caso mejor siendo n el numero de turnos
+	// pero en el caso peor O(n+h+n+a) siendo n el numero de turnos, h el numero de heroes, v el de villanos y a el de ataques.
+	bool heroe_ataca(Heroe const& h, std::string const& ataque, Villano const& v)
+	{
+		if (!villanos.count(v)) // Si no existe el villano excepcion.
+		{
+			throw std::invalid_argument("Villano inexistente");
+		}
+		if (!heroes.count(h)) // Si no existe el heroe excepcion.
+		{
+			throw std::invalid_argument("Heroe inexistente");
+		}
+		if (turnos.front() != h) // Si no es el turno del heroe excepcion.
+		{
+			throw std::invalid_argument("No es su turno");
+		}
+		if (!heroes[h].ataques.count(ataque)) // Si el heroe no tiene el ataque excepcion.
+		{
+			throw std::invalid_argument("Ataque no aprendido");
+		}
 
-		throw invalid_argument("Villano inexistente");
+		villanos[v].vida -= heroes[h].ataques[ataque];
 
-		throw invalid_argument("Heroe inexistente");
 
-		throw invalid_argument("No es su turno");
+		// Siguiente turno.
+		turnos.pop();
+		turnos.push(h);
 
-		throw invalid_argument("Ataque no aprendido");
 
+		// Si el villano se ha muerto.
+		if (villanos[v].vida <= 0)
+		{
+			villanos.erase(v); // Quitamos v de villanos.
+			death(v); // Reseteamos turnos.
+			return true;
+		}
+
+		return false;
 	}
 
+	// Coste: O(n) siendo n el numero de turnos.
+	void death(std::string p)
+	{
+		int siz = turnos.size();
+		for (int i = 0; i < siz; i++)
+		{
+			auto c = turnos.front();
+			if (c != p)
+			{
+				turnos.push(c);
+			}
+			turnos.pop();
+		}
+	}
 };
 
-
-bool resuelveCaso() { // No tacar nada de esta función!
-	string comando;
-	cin >> comando;
-	if (!cin) return false;
+bool resuelveCaso() { // No tacar nada de esta funcion!
+	std::string comando;
+	std::cin >> comando;
+	if (!std::cin) return false;
 
 	SistemaBatallas sistema;
 	Villano v; Heroe h;
-	string ataque; int ptos; int valor;
+	std::string ataque; int ptos; int valor;
 	while (comando != "FIN") {
 		try {
 			if (comando == "aparece_villano") {
-				cin >> v >> ptos >> valor;
+				std::cin >> v >> ptos >> valor;
 				sistema.aparece_villano(v, ptos, valor);
 			}
 			else if (comando == "aparece_heroe") {
-				cin >> h >> ptos;
+				std::cin >> h >> ptos;
 				sistema.aparece_heroe(h, ptos);
 			}
 			else if (comando == "aprende_ataque") {
-				cin >> h >> ataque >> valor;
+				std::cin >> h >> ataque >> valor;
 				sistema.aprende_ataque(h, ataque, valor);
 			}
 			else if (comando == "mostrar_ataques") {
-				cin >> h;
+				std::cin >> h;
 				auto ataques = sistema.mostrar_ataques(h);
-				cout << "Ataques de " << h << endl;
+				std::cout << "Ataques de " << h << std::endl;
 				for (auto& a : ataques) {
-					cout << a.first << " " << a.second << "\n";
+					std::cout << a.first << " " << a.second << "\n";
 				}
 			}
 			else if (comando == "mostrar_turnos") {
-				cout << "Turno: " << endl;
+				std::cout << "Turno: " << std::endl;
 				auto turnos = sistema.mostrar_turnos();
 				for (auto& t : turnos) {
-					cout << t.first << " " << t.second << "\n";
+					std::cout << t.first << " " << t.second << "\n";
 				}
 			}
 			else if (comando == "villano_ataca") {
-				cin >> v >> h;
+				std::cin >> v >> h;
 				bool derrotado = sistema.villano_ataca(v, h);
-				cout << v << " ataca a " << h << endl;
-				if (derrotado) cout << h << " derrotado" << endl;
+				std::cout << v << " ataca a " << h << std::endl;
+				if (derrotado) std::cout << h << " derrotado" << std::endl;
 			}
 			else if (comando == "heroe_ataca") {
-				cin >> h >> ataque >> v;
+				std::cin >> h >> ataque >> v;
 				bool derrotado = sistema.heroe_ataca(h, ataque, v);
-				cout << h << " ataca a " << v << endl;
-				if (derrotado) cout << v << " derrotado" << endl;
+				std::cout << h << " ataca a " << v << std::endl;
+				if (derrotado) std::cout << v << " derrotado" << std::endl;
 			}
 		}
 		catch (std::exception& e) {
-			cout << "ERROR: " << e.what() << "\n";
+			std::cout << "ERROR: " << e.what() << "\n";
 		}
-		cin >> comando;
+		std::cin >> comando;
 	}
 
-	cout << "---\n";
+	std::cout << "---\n";
 	return true;
 }
 
 //#define DOMJUDGE
 int main() {
 #ifndef DOMJUDGE
-	ifstream in("datos.txt");
+	std::ifstream in("datos.txt");
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif
 
