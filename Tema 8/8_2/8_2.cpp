a30ce5c10ae3ad72e8c6c26db5f80411989747ec
Paulalemany <palemany@ucm.es>
2024-12-03 16:04:28 +0100

    Luego hago la parte de los pacientes

--- Tema 8/8_2/8_2.cpp
+++ Tema 8/8_2/8_2.cpp
@@ -7,6 +7,7 @@
 #include <cassert>
 
 #include <set>
+#include <unordered_map>
 using namespace std;
 
 using medico = string;
@@ -21,24 +22,30 @@ struct fecha {
 
 class consultorio {
 private:
-    medico med;
+    //Vamos a hacer un mapa que ponga por cada medico las horas que tiene pilladas
+    unordered_map<medico, set<fecha>> medicos;
 
-    
-
-    set<medico> medicos;
+    //Hacemos otro mapa para los pacientes
+    //unordered_map<paciente, 
 
 public:
 
     void nuevoMedico(medico m) {
-        medicos.insert(m);  //El propio set solo lo anade si no esta
+        set<fecha> h;
+        medicos.insert({m, h});  //El propio map solo lo añade si no esta
     }
 
     void pideConsulta(paciente p, medico m, fecha f) {
 
         if (medicos.count(m) != 0) {
 
-            //Si tiene la fecha ya ocupada se lanza la excepcion
-            //throw exception("Fecha existente");
+            //Buscamos la fecha para ver si esta ocupada
+            if (medicos[m].count(f) == 0) { //La fecha no esta ocupada
+
+                //Le ponemos la fecha al medico
+                medicos[m].insert({ f });
+            }
+            else throw exception("Fecha ocupada");
         }
         else throw exception("Medico no existente");
     }
@@ -53,36 +60,36 @@ bool resuelve() {
     if (!cin) return false;
 
     string inst; medico med; paciente pac; int d, h, m; char c;
-    Consultorio con;
+    //Consultorio con;
 
     for (int i = 0; i < N; ++i) {
         try {
             cin >> inst;
             if (inst == "nuevoMedico") {
                 cin >> med;
-                con.nuevoMedico(med);
+                //con.nuevoMedico(med);
             }
             else if (inst == "pideConsulta") {
                 cin >> pac >> med >> d >> h >> c >> m;
-                con.pideConsulta(pac, med, fecha(d, h, m));
+               // con.pideConsulta(pac, med, fecha(d, h, m));
             }
             else if (inst == "siguientePaciente") {
                 cin >> med;
-                pac = con.siguientePaciente(med);
+                //pac = con.siguientePaciente(med);
                 cout << "Siguiente paciente doctor " << med << '\n';
                 cout << pac << '\n';
             }
             else if (inst == "atiendeConsulta") {
                 cin >> med;
-                con.atiendeConsulta(med);
+                //con.atiendeConsulta(med);
             }
             else if (inst == "listaPacientes") {
                 cin >> med >> d;
-                auto vec = con.listaPacientes(med, fecha(d, 0, 0));
+                //auto vec = con.listaPacientes(med, fecha(d, 0, 0));
                 cout << "Doctor " << med << " dia " << d << '\n';
-                for (auto p : vec) {
-                    cout << p.second << ' ' << p.first << '\n';
-                }
+                //for (auto p : vec) {
+                    //cout << p.second << ' ' << p.first << '\n';
+               // }
             }
             else
                 assert(false);
