14a914ce7c6f4219df4cab93f9da8045c1ea6696
pokoli <psierr01@ucm.es>
2025-01-02 13:43:45 +0100

    5

--- Eda5-1/Eda5-1.cpp
+++ Eda5-1/Eda5-1.cpp
@@ -1,38 +1,71 @@
-#include <iostream>
-
-#include "stack_eda.h"
-
-// Nombre del alumno .....
-// Usuario del Juez ......
-
+// PAULA SIERRA
+// EDA-GDV70
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 
+#include <string> // para el getline
 
+#include "stack_eda.h"
 
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
+bool resolver(std::string linea) 
+{
+    stack<char> chars;
 
+    int i = 0;
+    while (i < linea.size()) 
+    {
+        if (linea[i] == '(' || linea[i] == '[' || linea[i] == '{')
+        {
+            chars.push(linea[i]);
+        }
+        else if (linea[i] == ']') 
+        {
+            if (!chars.empty() && chars.top() == '[') {
+                chars.pop();
+            }
+            else chars.push(linea[i]);
+        }
+        else if (linea[i] == '}') 
+        {
+            if (!chars.empty() && chars.top() == '{') {
+                chars.pop();
+            }
+            else chars.push(linea[i]);
+        }
+        else if (linea[i] == ')') 
+        {
+            if (!chars.empty() && chars.top() == '(') {
+                chars.pop(); 
+            }
+            else chars.push(linea[i]);
+        }
 
+        i++;
+    }
+
+    return chars.empty();
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
+    std::string linea;
+    std::getline(std::cin, linea);
 
     if (!std::cin)
         return false;
 
-    TipoSolucion sol = resolver(datos);
+    bool sol = resolver(linea);
 
     // escribir sol
-
+    if(sol) std::cout << "SI" << std::endl;
+    else std::cout << "NO" << std::endl;
 
     return true;
-
 }
 
 int main() {
