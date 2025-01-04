2fd0cd71fc39dbc14b61ef6ff941d85f9137e730
ineprimo <ineprimo@ucm.es>
2025-01-04 19:38:38 +0100

    los dos primeros estan bien

---
+++ Examenes/ExamenFebrero21-Ej1/ExamenFebrero21-Ej1.cpp
@@ -0,0 +1,74 @@
+// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "queue_eda.h"
+
+using namespace std;
+
+// función que resuelve el problema
+void resolver(int datos) {
+
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    queue<int> datos;
+    int first, second;
+    int aux;
+    cin >> aux;
+
+    while (aux > -1) {
+        datos.push(aux);
+        cin >> aux;
+    }
+
+    cin >> first;
+    cin >> second;
+
+    if (!std::cin)
+        return false;
+
+    datos.cuela(first, second);
+
+    int size = datos.size();
+    // escribir sol
+    for (int i = 0; i < size; i++) {
+        cout << datos.front() << " ";
+        datos.pop();
+    }
+
+    cout << endl;
+
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
