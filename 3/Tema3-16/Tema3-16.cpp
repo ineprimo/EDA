4e73e0553a357705de277d97fd484a79910fa756
ineprimo <ineprimo@ucm.es>
2025-01-07 19:36:50 +0100

    esto resuelve 1 (una) cinta
    quels cujons

--- 3/Tema3-16/Tema3-16.cpp
+++ 3/Tema3-16/Tema3-16.cpp
@@ -30,59 +30,60 @@ void desmarcarCinta(vector<Cancion> cinta, const int i, const int k, const vecto
 
 
 void resolver(vector<vector<int>>& sol, vector<vector<int>>& mejorSol, const vector<Cancion>& canciones, int k, const int duracion,
-    vector<int>& espacioCinta, vector<bool>& cancionesUsadas, int& rizz, int& mejorRizz) {
+    vector<int>& espacioCinta, vector<bool>& cancionesUsadas, int cinta, int& rizz, int& mejorRizz) {
 
-    // i cancion
-    // k cinta
-
+    // i cinta
+    // k cancion
     for (int i = 0; i < canciones.size(); i++) {
+        if (k < canciones.size()) {
+            // si la cancion no ha sido usada
+            if (!cancionesUsadas[k]) {
 
-        // si la cancion no ha sido usada
-        if(!cancionesUsadas[i]) {
+                // la mete y la marca como usada
+                sol[cinta][k] = canciones[k].puntos;
 
-            // la mete y la marca como usada
-            sol[k][i] = canciones[i].puntos;
+                // la duracion actual se actualiza
+                espacioCinta[cinta] += canciones[k].duracion;
+                int dur = espacioCinta[cinta];
 
-            // la duracion actual se actualiza
-            espacioCinta[k] += canciones[i].duracion;
+                // la cancion se marca
+                cancionesUsadas[k] = true;
 
-            // la cancion se marca
-            cancionesUsadas[i] = true;
+                // se aumenta la puntuacion
+                rizz += canciones[k].puntos;
 
-            // se aumenta la puntuacion
-            rizz += canciones[i].puntos;
+                // si la cinta cara no esta llena
+                if (espacioCinta[cinta] <= duracion) {
+                    // si es solucion
+                    if (i == 1) {
+                        if (rizz > mejorRizz) {
+                            mejorRizz = rizz;
+                            mejorSol = sol;
+                        }
+                    }
+                    // si esta en la primera cinta
+                    else if (k < canciones.size()) {
+                        // poda
 
-            // si la cinta cara no esta llena
-            if (espacioCinta[k] <= duracion) {
-                // si es solucion
-                if (k == 1) {
-                    if (rizz > mejorRizz) {
-                        mejorRizz = rizz;
-                        mejorSol = sol;
+                        // backtracking a la siguiente cancion (?) pero misma cinta
+                        resolver(sol, mejorSol, canciones, k + 1, duracion, espacioCinta, cancionesUsadas, cinta, rizz, mejorRizz);
                     }
                 }
-                else {
-                    // poda
 
-                    // backtracking
-                    resolver(sol, mejorSol, canciones, k + 1, duracion, espacioCinta, cancionesUsadas, rizz, mejorRizz);
 
-                }
+                // la mete y la marca como usada
+                sol[cinta][k] = 0;
+
+                // la duracion actual se actualiza
+                espacioCinta[cinta] -= canciones[k].duracion;
+
+                // la cancion se marca
+                cancionesUsadas[k] = false;
+
+                // se aumenta la puntuacion
+                rizz -= canciones[k].puntos;
             }
-
-            // la mete y la marca como usada
-            sol[k][i] = 0;
-
-            // la duracion actual se actualiza
-            espacioCinta[k] -= canciones[i].duracion;
-
-            // la cancion se marca
-            cancionesUsadas[i] = false;
-
-            // se aumenta la puntuacion
-            rizz -= canciones[i].puntos;
         }
-
     }
 
 }
@@ -115,7 +116,7 @@ bool resuelveCaso() {
     int mejorRizz = 0;
     vector<int> espacioCinta(cinta);
 
-    resolver(sol, mejorSol, canciones, 0, duracion, espacioCinta, cancionesUsadas, rizz, mejorRizz);
+    resolver(sol, mejorSol, canciones, 0, duracion, espacioCinta, cancionesUsadas, 0, rizz, mejorRizz);
     cout << mejorRizz << endl;
 
     // Salida
